f8ea23378175ffa2bbae575e26f8b72692c56b5c
Yuki Ray Machida <ray@coelacanthdesign.com>
2020-01-22 20:30:12 -0800

    Added index1.js to JavaScript exercises folder

---
+++ exercises/js/index1.js
@@ -0,0 +1,14 @@
+/*
+Declare the following variables:
+- First name
+- Last name
+- Age
+- Address
+- Date of birth (using camel case)
+*/
+
+var firstName;
+var lastName;
+var age;
+var address;
+var dateOfBirth;
