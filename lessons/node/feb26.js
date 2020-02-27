b35a2e9cfc6f98bf24b91d4b96d5e06b7616a541
Yuki Ray Machida <ray@coelacanthdesign.com>
2020-02-26 19:41:51 -0800

    Added Feb 26 file to Node.js lessons folder

---
+++ lessons/node/feb26.js
@@ -0,0 +1,28 @@
+// Lesson 1
+
+/*
+const myES6Function = (parameter) => {
+    console.log('Parameter: ', parameter);
+}
+myES6Function('Hello?')
+*/
+
+
+// Lesson 2
+
+const getMessageArrowFunction = () => 'hi, this is a message';
+console.log(getMessageArrowFunction)
+
+// const getMessageArrowFunction = pumpkin => {
+//     return 'hi, this is a message ' + pumpkin;
+// }
+// console.log(getMessageArrowFunction('avocado'))
+
+// const getMessageArrowFunction = () => {
+//     console.log('hi, this is a message');
+//     return 'Now we have a return'
+// }
+// console.log(getMessageArrowFunction())
+
+// const getMessageArrowFunction = (fn) => fn();
+// console.log(getMessageArrowFunction(()=>'Hi'))
